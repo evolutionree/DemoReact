dc9b699171aa7e031b2213bf7b1143cbbf19c331
zhirongyuan <yuanzhirong@renqiankeji.com>
2017-12-04 14:37:58 +0800

    更改组件 treeNode  提供key属性 供动态获取对应key值

--- src/routes/CustomerHome/RelationTree/TreeNode.js
+++ src/routes/CustomerHome/RelationTree/TreeNode.js
@@ -6,17 +6,28 @@ import styles from './styles.less';
 import classnames from 'classnames';
 
 class TreeNode extends  React.Component {
+  static propTypes = {
+
+  };
+
+  static defaultProps = {
+    itemKey: 'id',
+    nameKey: 'name',
+    secondNameKey: 'username',
+    thirdNameKey: 'deptname'
+  };
+
   constructor(props) {
     super(props);
     this.state = {
-      data:this.props.data
-    }
+      data: this.props.data
+    };
   }
 
   componentWillReceiveProps(nextProps) {
     this.setState({
       data: nextProps.data
-    })
+    });
   }
 
 
@@ -25,26 +36,26 @@ class TreeNode extends  React.Component {
     if (this.state.data.children instanceof Array && this.state.data.children.length > 0)
     {
       this.state.data.children.map((item, index) => {
-        nodeControl.push(<TreeNode key={item.id}  data={item} custid={this.props.custid}/>);
+        nodeControl.push(<TreeNode key={item[this.props.itemKey]} data={item} custid={this.props.custid} />);
       });
     }
 
     const cls = classnames([styles.treeNodeItem, {
-      [styles.treeNodeItemActive]: this.state.data.id === this.props.custid  //当前客户
+      [styles.treeNodeItemActive]: this.state.data[this.props.itemKey] === this.props.custid  //当前客户
     }]);
     return (
       <li className={cls}>
         <div>
           <i></i>
-          <span>{this.state.data.name}</span>
-          <span className={styles.remarkInfo}>{this.state.data.username ? this.state.data.username : `(无)`}</span>
-          <span className={styles.remarkInfo}>{this.state.data.deptname ? this.state.data.deptname : `(无)`}</span>
+          <span>{this.state.data[this.props.nameKey]}</span>
+          <span className={styles.remarkInfo}>{this.state.data[this.props.secondNameKey] ? this.state.data[this.props.secondNameKey] : `(无)`}</span>
+          <span className={styles.remarkInfo}>{this.state.data[this.props.thirdNameKey] ? this.state.data[this.props.thirdNameKey] : `(无)`}</span>
         </div>
         <ul>
           {nodeControl}
         </ul>
       </li>
-    )
+    );
   }
-};
+}
 export default TreeNode;
