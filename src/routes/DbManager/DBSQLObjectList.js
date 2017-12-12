b6cdb2a0261730615d5330b2c4ac08c0da586c29
钟冠辉 <zhongguanhui@renqiankeji.com>
2017-12-12 14:15:59 +0800

    增加数据库管理

---
+++ src/routes/DbManager/DBSQLObjectList.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { connect } from 'dva';
+import { Select, Button, message, Modal, Breadcrumb } from 'antd';
+import DataGrid from './DataGrid';
+import * as _ from 'lodash';
+import DynamicTable from '../../components/DynamicTable';
+import Toolbar from '../../components/Toolbar';
+import Search from '../../components/Search';
+import styles from './styles.less';
+
+const Option = Select.Option;
+const columns = [{
+  title: '对象名称',
+  dataIndex: 'objname',
+  key: 'objname'
+}, {
+  title: '参数',
+  dataIndex: 'procparam',
+  key: 'procparam'
+}, {
+  title: '显示名称',
+  dataIndex: 'name',
+  key: 'name'
+}, {
+  title: '对象类型',
+  dataIndex: 'objtype_name',
+  key: 'objtype_name'
+}, {
+  title: '需要初始化',
+  dataIndex: 'needinitsql_name',
+  key: 'needinitsql_name'
+}, {
+  title: '操作',
+  key: 'action'
+}];
+
+function DBSQLObjectList({
+  queries,
+  total,
+  list,
+  search,
+  currentUser
+}) {
+  function exportData() {
+    window.open(`/api/excel/exportdata?TemplateType=0&FuncName=products_export&QueryParameters=${params}&UserId=${currentUser}`);
+  }
+  return (
+    <div className={styles.rightContent}>
+      {/*<div className={styles.subtitle}>{currentFullPath && currentFullPath.productsetname}</div>*/}
+
+      <Toolbar
+        actions={[
+
+        ]}
+      >
+        <Select value={queries.objecttype + ''} onChange={search.bind(null, 'objecttype')}>
+          <Option key="0">全部</Option>
+          <Option key="1">表格</Option>
+          <Option key="2">函数</Option>
+          <Option key="3">类型</Option>
+        </Select>
+        <Button >全部生成</Button>
+        <Button >导出</Button>
+        <Toolbar.Right>
+          <Search
+            value={queries.searchKey}
+            onSearch={search.bind(null, 'searchKey')}
+            placeholder="请输入对象名称"
+          />
+        </Toolbar.Right>
+      </Toolbar>
+
+      <DataGrid
+        columns={columns}
+        dataSource={list}
+        pagination={{
+          total,
+          current: +queries.pageIndex,
+          pageSize: +queries.pageSize,
+          onChange: search.bind(null, 'pageIndex'),
+          onShowSizeChange: (page, size) => { search('pageSize', size); }
+        }}
+      />
+    </div>
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    ...state.dbmanager,
+    currentUser: state.app.user.userid
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    search: (key, value) => {
+      dispatch({ type: 'dbmanager/search', payload: { [key]: value } });
+    }
+  };
+}
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DBSQLObjectList);
