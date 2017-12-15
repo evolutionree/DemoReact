c231da774316d1399630e28a18a2b93426b9e0c3
陈浩佳 <chenhaojia@renqiankeji.com>
2017-12-15 11:33:25 +0800

    产品数据过多时用弹窗控件

--- src/components/DynamicForm/controls/SelectProductModal.js
+++ src/components/DynamicForm/controls/SelectProductModal.js
@@ -5,15 +5,13 @@ import Search from '../../../components/Search';
 import Toolbar from '../../../components/Toolbar';
 import { queryProductData } from '../../../services/basicdata';
 import styles from './SelectUser.less';
+import SelectProductSerial from "./SelectProductSerial";
 
 class SelectProductModal extends Component {
   static propTypes = {
     visible: PropTypes.bool,
-    sourceId: PropTypes.string,
-    selected: PropTypes.arrayOf(PropTypes.shape({
-      name: PropTypes.string,
-      id: PropTypes.string
-    })),
+    data: PropTypes.object,
+    selected: PropTypes.arrayOf(PropTypes.string),
     onOk: PropTypes.func,
     onCancel: PropTypes.func,
     multiple: PropTypes.bool
@@ -27,52 +25,45 @@ class SelectProductModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      keyword: '',
-      currentSelected: props.selected.map(i => i.id),
+      currentSerial: '',
+      currentSelected: [...props.selected],
       list: [],
-      totalList: []
+      loading: false
     };
   }
 
   componentWillReceiveProps(nextProps) {
     if (!this.props.visible && nextProps.visible) {
       this.setState({
-        keyword: '',
-        currentSelected: nextProps.selected.map(i => i.id)
+        currentSerial: this.getDefaultSerial(nextProps),
+        currentSelected: [...nextProps.selected]
       }, this.fetchList);
     }
   }
 
+  onSerialChange = val => {
+    this.setState({ currentSerial: val }, this.fetchList);
+  };
+
   fetchList = () => {
-    this.setState({ loading: true });
-    queryProductData().then(result => {
-      this.setState({ loading: false });
-      const list = result.data.product;
-      this.setState({ list });
-    }, err => {
-      this.setState({ loading: false });
-      message.error(err.message || '加载数据失败');
-    });
+    const allProducts = this.props.data.products;
+    const list = (allProducts || []).filter(item => item.productsetid === this.state.currentSerial);
+    this.setState({ list });
+  };
+
+  getDefaultSerial = (props) => {
+    const serials = props.data.productserial;
+    return serials && serials[0].productsetid;
   };
 
   handleOk = () => {
-    const { currentSelected, list } = this.state;
-    const arrIdName = currentSelected
-      .map(id => _.find(list, ['productid', id]))
-      .map(item => ({ id: item.productid, name: item.productname }));
-    this.props.onOk(arrIdName);
-  };
-
-  onSearch = (keyword) => {
-    this.setState({
-      keyword,
-      currentSelected: this.props.multiple ? this.state.currentSelected : []
-    }, this.fetchList);
+    this.props.onOk(this.state.currentSelected);
   };
 
   selectAll = () => {
+    const ids = _.map(this.state.list, 'productid');
     this.setState({
-      currentSelected: _.map(this.state.list, 'productid')
+      currentSelected: _.union(this.state.currentSelected, ids)
     });
   };
 
@@ -102,10 +93,15 @@ class SelectProductModal extends Component {
     this.setState({ currentSelected: [] });
   };
 
+  getSelectedItems = () => {
+    const allProducts = this.props.data.products;
+    return this.state.currentSelected.map(id => _.find(allProducts, ['productid', id])).filter(i => !!i);
+  };
+
   render() {
     const { visible, onCancel, multiple } = this.props;
     const { currentSelected, list } = this.state;
-    const currentSelectedItem = list.length ? currentSelected.map(id => _.find(list, ['productid', id])) : [];
+    const selectedItems = this.getSelectedItems();
     return (
       <Modal
         title="选择产品"
@@ -114,22 +110,19 @@ class SelectProductModal extends Component {
         onCancel={onCancel}
         wrapClassName={multiple ? 'ant-modal-custom-large' : ''}
       >
-        {/*<Toolbar>*/}
-          {/*<Search*/}
-            {/*width="200px"*/}
-            {/*value={this.state.keyword}*/}
-            {/*onSearch={this.onSearch}*/}
-            {/*placeholder="请输入关键字"*/}
-          {/*>*/}
-            {/*搜索*/}
-          {/*</Search>*/}
-        {/*</Toolbar>*/}
+        <Toolbar>
+          <SelectProductSerial
+            wrapStyle={{ width: '200px' }}
+            value={this.state.currentSerial}
+            onChange={this.onSerialChange}
+          />
+        </Toolbar>
         <Spin spinning={this.state.loading}>
           {multiple ? (
             <Row gutter={20}>
               <Col span={11}>
                 <ul className={styles.userlist}>
-                  {this.state.list.map(item => (
+                  {list.map(item => (
                     <li key={item.productid} onClick={this.select.bind(this, item)}>
                       <span title={item.productname}>{item.productname}</span>
                     </li>
@@ -144,7 +137,7 @@ class SelectProductModal extends Component {
               </Col>
               <Col span={11}>
                 <ul className={styles.userlist}>
-                  {currentSelectedItem.map(item => (
+                  {selectedItems.map(item => (
                     <li key={item.productid}>
                       <span title={item.productname}>{item.productname}</span>
                       <Icon type="close" onClick={this.remove.bind(this, item)} />
@@ -155,7 +148,7 @@ class SelectProductModal extends Component {
             </Row>
           ) : (
             <ul className={styles.userlist}>
-              {this.state.list.map(item => {
+              {list.map(item => {
                 const cls = currentSelected[0] === item.productid ? styles.highlight : '';
                 return (
                   <li key={item.productid} onClick={this.selectSingle.bind(this, item)} className={cls}>
