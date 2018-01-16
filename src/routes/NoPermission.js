b2086e6e098982cadc188efe8e49740196b38085
zhirongyuan <yuanzhirong@renqiankeji.com>
2018-01-16 11:23:47 +0800

    no message

--- src/routes/NoPermission.js
+++ src/routes/NoPermission.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 function NoPermissionPage() {
   return (
-    <div>
+    <div style={{ textAlign: 'center', marginTop: '100px', fontSize: '16px' }}>
       系统管理员尚未为您分配系统权限，请联系系统管理员
     </div>
   );
