7aa96d831df5b1192fb00b522d1edde34d1c9ce2
陈浩佳 <chenhaojia@renqiankeji.com>
2018-03-12 14:55:59 +0800

    二维码文本

--- src/components/DynamicForm/controls/InputText.js
+++ src/components/DynamicForm/controls/InputText.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
+import QRCode from 'qrcode.react';
 import { createNormalInput } from './utils';
 import { DefaultTextView } from '../DynamicFieldView';
+import { Icon, Popover } from 'antd'
 
 const InputText = createNormalInput('text');
 
 InputText.View = (props) => {
-  const { value, encrypted } = props;
+  const { value, encrypted, scanner } = props;
   if (value && encrypted) {
     return <div style={{ wordWrap: 'break-word', whiteSpace: 'normal' }}>********</div>;
+  } else if (value && scanner) {
+    return (
+      <div>
+        <span style={{ verticalAlign: 'middle' }}>{value}</span>
+        <Popover
+          title="二维码"
+          trigger="click"
+          content={(
+            <QRCode value={value} size={160} fgColor="#666" />
+          )}
+        >
+          <Icon
+            style={{ marginLeft: '5px', cursor: 'pointer', verticalAlign: 'middle' }}
+            type="qrcode"
+          />
+        </Popover>
+      </div>
+    );
   } else {
     return <DefaultTextView {...props} />;
   }
