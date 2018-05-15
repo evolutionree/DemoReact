3bff7a7727e302a59c903d9cdbaebd1643bb92ce
yuanzhirong <yuanzhirong@renqiankeji.com>
2018-05-15 15:33:10 +0800

    外勤设置暂未开发完，已屏蔽

--- src/routes/EntityHome/index.js
+++ src/routes/EntityHome/index.js
@@ -27,7 +27,7 @@ function EntityConfig({
     <LinkTab key="4" to={`/entity-config/${entityId}/${entityType}/menus`}>常用筛选列表</LinkTab>,
     <LinkTab key="5" to={`/entity-config/${entityId}/${entityType}/tabs`}>页签设置</LinkTab>,
     <LinkTab key="6" to={`/entity-config/${entityId}/${entityType}/extendconfig`}>扩展配置</LinkTab>,
-    <LinkTab key="7" to={`/entity-config/${entityId}/${entityType}/attenceset`}>外勤设置</LinkTab>
+    {/*<LinkTab key="7" to={`/entity-config/${entityId}/${entityType}/attenceset`}>外勤设置</LinkTab>*/}
   ];
   const tabbar = (
     <LinkTab.Group>
