e5c3e3560c22cb135ae4ee35088b18b1cb7fe2fe
yuanzhirong <yuanzhirong@renqiankeji.com>
2018-08-01 14:58:14 +0800

    【bug-3】解决 表单，如果设置横向显示，查看表单未生效

--- src/components/DynamicForm/DynamicFormView.js
+++ src/components/DynamicForm/DynamicFormView.js
@@ -16,8 +16,33 @@ class DynamicFormView extends React.Component {
       controltype: PropTypes.number.isRequired,
       fieldconfig: PropTypes.object.isRequired
     })),
-    value: PropTypes.object.isRequired
+    value: PropTypes.object.isRequired,
+    horizontal: PropTypes.bool
   };
+
+  static defaultProps = {
+    horizontal: false
+  };
+
+  getFormLayout = () => {
+    if (this.props.horizontal) return 'horizontal';
+    const { fields } = this.props;
+    let layout = 'vertical';
+    if (fields && fields[0]) {
+      const { fieldconfig } = fields[0];
+      if (fieldconfig && fieldconfig.style === 0) {
+        layout = 'horizontal';
+      }
+    }
+    return layout;
+  };
+
+  getFormItemLayout = () => {
+    return this.getFormLayout() === 'horizontal'
+      ? { labelCol: { span: 4 }, wrapperCol: { span: 20 } }
+      : null;
+  };
+
   renderFields = fields => {
     const { value, entityId, entityTypeId } = this.props;
     return fields.filter(field => field.controltype !== 30).map(field => {
@@ -27,6 +52,7 @@ class DynamicFormView extends React.Component {
           key={fieldname}
           colon={false}
           label={displayname}
+          {...this.getFormItemLayout(field.fieldname)}
           style={(fieldconfig.isVisible !== 1 || field.fieldconfig.isVisibleJS === 0) ? { display: 'none' } : {}}
         >
           <DynamicFieldView
