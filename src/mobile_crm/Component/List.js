9391fae01850194a3f4504072b433371cc6bc4e6
yuanzhirong <yuanzhirong@renqiankeji.com>
2018-08-06 15:31:35 +0800

    钉钉CRM

--- src/mobile_crm/Component/List.js
+++ src/mobile_crm/Component/List.js
@@ -11,14 +11,14 @@ function List({
                 style
               }) {
   return (
-    <ul>
+    <ul className={styles.listWrap}>
       <li>
         <img src="/img_img_card.png" />
-        <div>
+        <div className={styles.left}>
           <div>山东沙发里看见快乐发神经</div>
           <div>2018232393444</div>
         </div>
-        <div>
+        <div className={styles.right}>
           <div>系统管理员</div>
           <div>核准客户</div>
         </div>
