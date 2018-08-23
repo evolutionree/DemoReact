9ccb7130da2142d6fc2ec12ab223eb7e3111bf03
yuanzhirong <yuanzhirong@renqiankeji.com>
2018-08-23 16:02:44 +0800

    【优化】模板打印 增加Loading等待效果

--- src/routes/modals/PrintEntityModal/PrintEntityModal.js
+++ src/routes/modals/PrintEntityModal/PrintEntityModal.js
@@ -1,6 +1,6 @@
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'dva';
-import { Form, Modal, Radio, message } from 'antd';
+import { Form, Modal, Radio, message, Spin } from 'antd';
 import { printEntity } from '../../../services/printTemplate';
 
 class PrintEntityModal extends Component {
@@ -17,7 +17,8 @@ class PrintEntityModal extends Component {
     super(props);
     this.state = {
       selectedTemplate: props.templateList.length ? props.templateList[0].recid : null,
-      selectedOutput: props.outputTypes.length ? props.outputTypes[0].type : null
+      selectedOutput: props.outputTypes.length ? props.outputTypes[0].type : null,
+      lodingPrint: false
     };
   }
 
@@ -52,10 +53,19 @@ class PrintEntityModal extends Component {
       recid: this.props.recordId,
       templateid: this.state.selectedTemplate
     };
+    this.setState({
+      lodingPrint: true
+    });
     printEntity(params).then(result => {
       window.open(`api/PrintForm/exportfile?fileid=${result.data.fileid}&fileName=${result.data.filename}`);
+      this.setState({
+        lodingPrint: false
+      });
       this.props.close && this.props.close();
     }, err => {
+      this.setState({
+        lodingPrint: false
+      });
       message.error(err.message || '打印失败');
     });
   };
@@ -68,22 +78,24 @@ class PrintEntityModal extends Component {
         onCancel={this.handleCancel}
         onOk={this.handleOk}
       >
-        <Form>
-          <Form.Item label="可用模板">
-            <Radio.Group value={this.state.selectedTemplate} onChange={e => this.setState({ selectedTemplate: e.target.value })}>
-              {this.props.templateList.map(item => (
-                <Radio value={item.recid} key={item.recid}>{item.templatename}</Radio>
-              ))}
-            </Radio.Group>
-          </Form.Item>
-          <Form.Item label="输出方式">
-            <Radio.Group value={this.state.selectedOutput} onChange={e => this.setState({ selectedOutput: e.target.value })}>
-              {this.props.outputTypes.map(item => (
-                <Radio value={item.type} key={item.type}>{item.label}</Radio>
-              ))}
-            </Radio.Group>
-          </Form.Item>
-        </Form>
+        <Spin spinning={this.state.lodingPrint}>
+          <Form>
+            <Form.Item label="可用模板">
+              <Radio.Group value={this.state.selectedTemplate} onChange={e => this.setState({ selectedTemplate: e.target.value })}>
+                {this.props.templateList.map(item => (
+                  <Radio value={item.recid} key={item.recid}>{item.templatename}</Radio>
+                ))}
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item label="输出方式">
+              <Radio.Group value={this.state.selectedOutput} onChange={e => this.setState({ selectedOutput: e.target.value })}>
+                {this.props.outputTypes.map(item => (
+                  <Radio value={item.type} key={item.type}>{item.label}</Radio>
+                ))}
+              </Radio.Group>
+            </Form.Item>
+          </Form>
+        </Spin>
       </Modal>
     );
   }
