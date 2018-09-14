b31e3dcd86005026b7bb54072bd4a571d4ac6b14
周靖人 <zhoujingren@wxchina.com>
2018-09-14 10:04:46 +0800

    approvalnotice组件的修改

--- src/routes/Desk/components/ApprovalNotice/index.js
+++ src/routes/Desk/components/ApprovalNotice/index.js
@@ -1,28 +1,46 @@
 /**
  * jingren
  */
-import React, { PureComponent } from 'react';
-import { Button, Select } from 'antd';
+import React, { PropTypes, PureComponent } from 'react';
+import { Tabs, Badge } from 'antd';
 import styles from './index.less';
 import { Link } from 'dva/router';
 import { queryNoticeList } from '../../../../services/approvalNotice';
 
-const Option = Select.Option;
+const { TabPane } = Tabs;
+
 const optionList = [
-  { key: '-1', name: '全部1' },
-  { key: '0', name: '审批中' },
-  { key: '1', name: '通过' },
-  { key: '2', name: '不通过' }
-];
+  { key: '1',
+    name: '待处理',
+    count: '8',
+    data: {
+      name: '发起人姓名',
+      lcName: '审批流程名称',
+      result: '待我审批',
+      person: '当前节点人'
+    }
+  }, {
+    key: '2',
+    name: '通知消息',
+    count: '233',
+    data: {
+      name: '发起人姓名',
+      lcName: '审批流程名称',
+      result: '待我审批',
+      person: '当前节点人'
+    }
+}];
 
 class ApprovalNotice extends PureComponent {
+  static propTypes = {
+    height: PropTypes.string.isRequired,
+  }
 
   state = {
     data: null,
-    emptyText: '暂无新的审批通知 ',
-    clear: '清空',
-    total: '查看更多',
-    title: '审批通知',
+    tabState: '1', //tabs 状态
+    title: '审批',
+    more: '查看全部',
     params: {
       entityId: "00000000-0000-0000-0000-000000000001",
       isAdvanceQuery: 1,
@@ -41,13 +59,7 @@ class ApprovalNotice extends PureComponent {
     this.fetchList(params);
   }
 
-  onClear = () => {
-    if (!!this.state.data && confirm('确定清空吗？清空无法恢复，请谨慎操作！')) {
-      this.setState({ data: null, emptyText: '已清空审批通知' })
-    }
-  }
-
-  onSelectChange = type => {
+  onTabsChange = type => {
     const { params } = this.state;
     const auditstatus = (type === '-1') ? null : parseInt(type);
     const mergeParams = {
@@ -60,74 +72,88 @@ class ApprovalNotice extends PureComponent {
     this.fetchList(mergeParams);
   };
 
+  onReadMsg = idx => {
+    
+  }
+
   fetchList = (params) => {
     this.setState({ params });
 
-    queryNoticeList(params).then(res => {
-      this.setState({ data: res.data })
-    })
+    queryNoticeList(params)
+    .then(res => this.setState({ data: res.data }))
     .catch(err => console.log(err))
   }
 
   render() {
     const { height = 200 } = this.props;
-    const { clear, emptyText, total, title, data } = this.state;
+    const { tabState, more, title, data } = this.state;
 
     const listDom = !!data && (
-      <div className={styles.contents}>
+      <div className={styles.container}>
         {
           data.pagedata.map((item, index) => {
             return (
-              <div className={styles.list} key={index}>
+              <div className={styles.list} key={index} onClick={this.onReadMsg.bind(this,index)}>
                 <Link to='/'>
                   <font></font>
                   <span>{`【${item.auditstatus_name}】`}</span>
                   <span className={styles.text}>
-                    我发起的客户审批，<span className={styles.weight}>{`${item.auditstatus_name}`}！</span>
+                    {item.data.name}发起的{item.data.lcname}，
+                    {
+                      item.id === '3' ? 
+                      `等待${item.data.person}审批！`
+                      :
+                      <span className={styles.weight}>{`${item.auditstatus_name}`}！</span>
+                    }
                   </span>
                 </Link>
               </div>
             );
           })
         }
-        {
-          data.pagedata.length > 5 ?
-            <div className={styles.footer}>
-              {`共${data.pagecount[0].total}条审批通知，点击`}
-              <Link to='/'><span>{total}</span></Link>
-            </div>
-            : null
-        }
       </div>
     )
 
-    const showList = !!data ? //data是否存在
-      listDom
-      :
-      <div className={styles.notNewMsg}>{emptyText}</div>; //没有数据
+    const showList = (!!data) ?  listDom  :
+    <div className={styles.notNewMsg}>{tabState === '1' ? '暂无新的待处理审批！' : '暂无新的审批通知！'}</div>
+    ;
 
     return (
-      <div className={styles.contains} style={{ height: height }}>
+      <div className={styles.contains} style={{ maxHeight: height }}>
         <div className={styles.header}>
-          <div>
-            <span className={styles.title}>{title}</span>
-            <Button onClick={this.onClear}>{clear}</Button>
-          </div>
-          <Select
-            style={{ minWidth: '110px' }}
-            defaultValue='-1'
-            onChange={this.onSelectChange}
+          <Tabs defaultActiveKey={tabState}
+          animated
+          tabBarExtraContent={<span className={styles.title}>{title}</span>}
+          onTabClick={this.onTabsChange}
           >
             {
-              Array.isArray(optionList)
-                ?
-                optionList.map(item => <Option key={item.key}>{item.name}</Option>)
-                :
-                null
+              Array.isArray(optionList) ?
+                optionList.map(item => {
+                  return (
+                    <TabPane 
+                      tab={
+                        <Badge className={styles.badge} count={item.count} title={`共${item.count}条信息`}>
+                          <span className={styles.tabText}>{item.name}</span>
+                        </Badge>
+                      }
+                      key={item.key}
+                    >
+                      <div className={styles.warp} >
+                      {showList}
+                      {
+                        (!!data) && data.pagedata.length >= 10 ?
+                          <div className={styles.footer}>
+                            <Link to='/affair-list'><span>{more}</span></Link>
+                          </div>
+                          : null
+                      }
+                      </div>
+                    </TabPane>
+                  )
+                }) : null
             }
-          </Select>
+          </Tabs>
         </div>
-        <div className={styles.warp} style={{ height: `${height - 60}px` }}>{showList}</div>
       </div>
     );
   }
