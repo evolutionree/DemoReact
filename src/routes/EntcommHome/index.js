bdbb9f1ea2a239ecd0791b53dfc1760baff7b888
周靖人 <zhoujingren@wxchina.com>
2018-11-12 17:18:41 +0800

    no message

--- src/routes/EntcommHome/index.js
+++ src/routes/EntcommHome/index.js
@@ -19,22 +19,22 @@ function EntcommHome({
   location,
   mainFieldsConfig,
   toggleFollow,
-                       firstLoad,
-                       stopAutoLink
+  firstLoad,
+  stopAutoLink
 }) {
   const entityId = params.entityId;
   const recordId = params.recordId;
   const tabbar = (
     <LinkTab.Group>
-{/*   <LinkTab to={`/entcomm/${entityId}/${recordId}/activities`}>动态</LinkTab>
+      {/*   <LinkTab to={`/entcomm/${entityId}/${recordId}/activities`}>动态</LinkTab>
       <LinkTab to={`/entcomm/${entityId}/${recordId}/info`}>基础信息</LinkTab>
       <LinkTab to={`/entcomm/${entityId}/${recordId}/docs`}>文档</LinkTab>
       {'239a7c69-8238-413d-b1d9-a0d51651abfa' === entityId && <LinkTab to={`/entcomm/${entityId}/${recordId}/receivepay`}>回款</LinkTab>}
       {'f9db9d79-e94b-4678-a5cc-aa6e281c1246' === entityId && <LinkTab to={`/entcomm/${entityId}/${recordId}/relationtree`}>客户关系</LinkTab>}*/}
       {[...relTabs.map(t => (
         t.entitytaburl ?
-        <LinkTab key={t.relid} to={`/entcomm/${entityId}/${recordId}/${t.entitytaburl}`}><IntlText name="relname" value={t} /></LinkTab> :
-        <LinkTab key={t.relid} to={`/entcomm/${entityId}/${recordId}/rel/${t.relid}/${t.relentityid}`}><IntlText name="relname" value={t} /></LinkTab>
+          <LinkTab key={t.relid} to={`/entcomm/${entityId}/${recordId}/${t.entitytaburl}`}><IntlText name="relname" value={t} /></LinkTab> :
+          <LinkTab key={t.relid} to={`/entcomm/${entityId}/${recordId}/rel/${t.relid}/${t.relentityid}`}><IntlText name="relname" value={t} /></LinkTab>
       ))]}
       {'9834d2bc-084a-49de-bbbf-02f35c191b64' === entityId && <LinkTab to={`/entcomm/${entityId}/${recordId}/relationschema`}>关系图</LinkTab>}
     </LinkTab.Group>
@@ -58,22 +58,22 @@ function EntcommHome({
     </span>
   );
 
-    const routhPath = relTabs.map(item => {
-      if (item.entitytaburl) {
-        return `/entcomm/${entityId}/${recordId}/${item.entitytaburl}`;
-      } else {
-        return `/entcomm/${entityId}/${recordId}/rel/${item.relid}/${item.relentityid}`;
-      }
-    });
-
-    if (firstLoad && routhPath.length > 0) {
-      if (location.pathname !== routhPath[0]) {
-        stopAutoLink();
-        hashHistory.push(routhPath[0]);
-      } else { //TODO: 第一个页签  跟 路由配置文件对应的首个路由一致  则也需设置 firstLoad： false
-        stopAutoLink();
-      }
+  const routhPath = relTabs.map(item => {
+    if (item.entitytaburl) {
+      return `/entcomm/${entityId}/${recordId}/${item.entitytaburl}`;
+    } else {
+      return `/entcomm/${entityId}/${recordId}/rel/${item.relid}/${item.relentityid}`;
     }
+  });
+
+  if (firstLoad && routhPath.length > 0) {
+    if (location.pathname !== routhPath[0]) {
+      stopAutoLink();
+      hashHistory.push(routhPath[0]);
+    } else { //TODO: 第一个页签  跟 路由配置文件对应的首个路由一致  则也需设置 firstLoad： false
+      stopAutoLink();
+    }
+  }
 
   return (
     <Page
