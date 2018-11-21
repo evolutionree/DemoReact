86235f41030d678e8a029d939f5ffb3cae841a3e
yuanzhirong <yuanzhirong@renqiankeji.com>
2018-11-21 11:17:28 +0800

    修改 表单电话号码 、手机号  校验规则

--- src/components/DynamicForm/controls/InputMobile.js
+++ src/components/DynamicForm/controls/InputMobile.js
@@ -2,10 +2,10 @@ import { createNormalInput } from './utils';
 
 const InputText = createNormalInput('text', {
   filter: inputValue => {
-    return inputValue.replace(/[^\d]/g, '');
+    return inputValue.replace(/[^\d^\-]+/g, '');
   },
   props: {
-    maxLength: '11'
+
   }
 });
 
