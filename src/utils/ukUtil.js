06f744c506f88fee899057cbc209d6f73b1e8ff2
周靖人 <zhoujingren@wxchina.com>
2019-04-18 12:41:41 +0800

    no message

--- src/utils/ukUtil.js
+++ src/utils/ukUtil.js
@@ -1,3 +1,5 @@
+import Fingerprint from 'fingerprintjs';
+
 export function downloadFile(url) { //下载文件
   // 创建隐藏的可下载链接
   let eleLink = document.createElement('a');
@@ -12,34 +14,6 @@ export function downloadFile(url) { //下载文件
   document.body.removeChild(eleLink);
 }
 
-function bin2hex(s) {
-  let o = '';
-  let n;
-  let string = s + '';
-  for (let i = 0; i < string.length; i++) {
-    n = string.charCodeAt(i).toString(16);
-    o += n.length < 2 ? '0' + n : n;
-  }
-  return o;
+export function getBrowserUUID() {
+  return new Fingerprint({ canvas: true }).get();
 }
-
-export function getBrowserUUID(domain) {
-  let canvas = document.createElement('canvas');
-  let ctx = canvas.getContext('2d');
-  let txt = domain;
-  ctx.textBaseline = 'top';
-  ctx.font = "14px 'Arial'";
-  ctx.textBaseline = 'tencent';
-  ctx.fillStyle = '#f60';
-  ctx.fillRect(125, 1, 62, 20);
-  ctx.fillStyle = '#069';
-  ctx.fillText(txt, 2, 15);
-  ctx.fillStyle = 'rgba(102, 204, 0, 0.7)';
-  ctx.fillText(txt, 4, 17);
-
-  let b64 = canvas.toDataURL().replace('data:image/png;base64,', '');
-  let bin = atob(b64);
-  let crc = bin2hex(bin.slice(-16, -12));
-  return crc;
-}
-
