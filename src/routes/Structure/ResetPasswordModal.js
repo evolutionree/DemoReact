8e1742823448b2a4dc43b451f0d224a8d58107a4
周靖人 <zhoujingren@wxchina.com>
2019-06-19 16:02:40 +0800

    任务: 【ID1005424】【密码】密码上下一致性判断逻辑错误

--- src/routes/Structure/ResetPasswordModal.js
+++ src/routes/Structure/ResetPasswordModal.js
@@ -1,128 +1,137 @@
-import React from 'react';
-import _ from 'lodash';
-import { Form, Input, Modal } from 'antd';
-import { connect } from 'dva';
-import styles from './ResetPasswordModal.less';
+import React from 'react'
+import _ from 'lodash'
+import { Form, Input, Modal } from 'antd'
+import { connect } from 'dva'
+import styles from './ResetPasswordModal.less'
 
-const FormItem = Form.Item;
+const FormItem = Form.Item
 
-function ResetPasswordModal({
-  form: {
-    getFieldDecorator,
-    validateFields,
-    getFieldValue,
-    resetFields,
-    setFieldsValue
-  },
+function ResetPasswordModal ({
+  form: { getFieldDecorator, validateFields, getFieldValue, resetFields, setFieldsValue },
   showModals,
   currentItems,
   revertPassword,
   cancel
 }) {
-  function handleOk() {
+  function handleOk () {
     validateFields((err, values) => {
-      if (err) return;
+      if (err) return
       Modal.confirm({
         title: '确认重置用户密码？',
-        onOk() {
-          revertPassword(_.pick(values, ['accountpwd']));
+        onOk () {
+          revertPassword(_.pick(values, ['accountpwd']))
         }
-      });
-    });
+      })
+    })
   }
-  function handelCancel() {
-    cancel();
+  function handelCancel () {
+    cancel()
   }
-  function checkSameChar(rule, value, callback) {
-    if (!value) return callback();
-    if (value.length < 6) return callback();
+  function checkSameChar (rule, value, callback) {
+    if (!value) return callback()
+    if (value.length < 6) return callback()
 
-    let isAllSameChar = true;
-    const chars = value.split('');
+    let isAllSameChar = true
+    const chars = value.split('')
     for (let i = 1; i < chars.length; i += 1) {
-      if (chars[i] !== chars[0]) isAllSameChar = false;
+      if (chars[i] !== chars[0]) isAllSameChar = false
     }
-    const confirmValue = getFieldValue('confirm');
+    const confirmValue = getFieldValue('confirm')
 
     if (isAllSameChar) {
-      callback('密码不能全部相同');
+      callback('密码不能全部相同')
     } else if (confirmValue && confirmValue.length >= 6 && value !== confirmValue) {
-      callback('两次填写的密码不一致');
+      callback('两次填写的密码不一致')
     } else {
-      if (confirmValue === value) setTimeout(setFieldsValue({ confirm: confirmValue }), 0);
-      callback();
+      if (confirmValue === value) setTimeout(setFieldsValue({ confirm: confirmValue }), 0)
+      callback()
     }
   }
-  function checkPasswordConfirm(rule, value, callback) {
+  function checkPasswordConfirm (rule, value, callback) {
     if (value && value !== getFieldValue('accountpwd')) {
-      callback('两次填写的密码不一致');
+      callback('两次填写的密码不一致')
     } else {
-      callback();
+      callback()
     }
   }
 
   return (
     <Modal
-      title="修改密码"
+      title='修改密码'
       visible={/resetPassword/.test(showModals)}
       onOk={handleOk}
       onCancel={handelCancel}
       afterClose={resetFields}
     >
-      <div className={styles.title} >选中人员（共{currentItems.length}人）</div>
-      <div className={styles.well} >
-        {currentItems.map(u => u.username).join('、')}
-      </div>
+      <div className={styles.title}>选中人员（共{currentItems.length}人）</div>
+      <div className={styles.well}>{currentItems.map(u => u.username).join('、')}</div>
       <Form>
-        <FormItem label="新密码">
+        <FormItem label='新密码'>
           {getFieldDecorator('accountpwd', {
             initialValue: '',
             normalize: value => (value ? value.trim() : value),
-            rules: [{
-              required: true,
-              pattern: /^.{6,16}$/,
-              message: '密码为6-16位字符'
-            }, {
-              validator: checkSameChar
-            }]
+            rules: [
+              {
+                required: true,
+                pattern: /^.{6,16}$/,
+                message: '密码为6-16位字符'
+              },
+              {
+                validator: checkSameChar
+              }
+            ]
           })(
-            <Input onPaste={(e) => {
-              e.preventDefault();
-            }} type="password" maxLength="16" placeholder="请输入6-16位新密码" />
+            <Input
+              onPaste={e => {
+                e.preventDefault()
+              }}
+              type='password'
+              maxLength='16'
+              placeholder='请输入6-16位新密码'
+            />
           )}
         </FormItem>
-        <FormItem label="确认密码">
+        <FormItem label='确认密码'>
           {getFieldDecorator('confirm', {
             initialValue: '',
             normalize: value => (value ? value.trim() : value),
             trigger: 'onChange',
-            rules: [{
-              required: true, message: '请输入确认密码'
-            }, {
-              validator: checkPasswordConfirm
-            }]
+            rules: [
+              {
+                required: true,
+                message: '请输入确认密码'
+              },
+              {
+                validator: checkPasswordConfirm
+              }
+            ]
           })(
-            <Input onPaste={(e) => {
-              e.preventDefault();
-            }} type="password" maxLength="16" placeholder="请输入确认密码" />
+            <Input
+              onPaste={e => {
+                e.preventDefault()
+              }}
+              type='password'
+              maxLength='16'
+              placeholder='请输入确认密码'
+            />
           )}
         </FormItem>
       </Form>
     </Modal>
-  );
+  )
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps (dispatch) {
   return {
     revertPassword: data => {
-      dispatch({ type: 'structure/revertPassword', payload: data });
+      dispatch({ type: 'structure/revertPassword', payload: data })
     },
     cancel: () => {
-      dispatch({ type: 'structure/showModals', payload: '' });
+      dispatch({ type: 'structure/showModals', payload: '' })
     }
   }
 }
 export default connect(
   state => state.structure,
   mapDispatchToProps
-)(Form.create()(ResetPasswordModal));
+)(Form.create({})(ResetPasswordModal))
--- src/routes/modals/ModifyPwdModal.js
+++ src/routes/modals/ModifyPwdModal.js
@@ -1,116 +1,131 @@
-import React from 'react';
-import { Form, Input, Modal } from 'antd';
-import { connect } from 'dva';
-import _ from 'lodash';
+import React from 'react'
+import { Form, Input, Modal } from 'antd'
+import { connect } from 'dva'
+import _ from 'lodash'
 
-const FormItem = Form.Item;
+const FormItem = Form.Item
 
-function ModifyPwdModal({
-    form: {
-      getFieldDecorator,
-      validateFields,
-      getFieldValue,
-      resetFields
-    },
-    showModals,
-    modifyPwd,
-    cancel
-  }) {
-  function handleOk() {
+function ModifyPwdModal ({
+  form: { getFieldDecorator, validateFields, getFieldValue, resetFields, setFieldsValue },
+  showModals,
+  modifyPwd,
+  cancel
+}) {
+  function handleOk () {
     validateFields((err, values) => {
-      if (err) return;
-      modifyPwd(_.pick(values, ['orginpwd', 'accountpwd']));
-    });
+      if (err) return
+      modifyPwd(_.pick(values, ['orginpwd', 'accountpwd']))
+    })
   }
-  function handelCancel() {
-    resetFields();
-    cancel();
+  function handelCancel () {
+    resetFields()
+    cancel()
   }
-  function checkSameChar(rule, value, callback) {
-    if (!value) return callback();
-    if (value.length < 6) return callback();
-    let isAllSameChar = true;
-    const chars = value.split('');
-    for (let i = 1; i < chars.length; i++) {
-      if (chars[i] !== chars[0]) isAllSameChar = false;
+  function checkSameChar (rule, value, callback) {
+    if (!value) return callback()
+    if (value.length < 6) return callback()
+    let isAllSameChar = true
+    const chars = value.split('')
+    for (let i = 1; i < chars.length; i += 1) {
+      if (chars[i] !== chars[0]) isAllSameChar = false
     }
+    const confirmValue = getFieldValue('confirm')
+
     if (isAllSameChar) {
-      callback('密码不能全部相同');
+      callback('密码不能全部相同')
+    } else if (confirmValue && confirmValue.length >= 6 && value !== confirmValue) {
+      callback('两次填写的密码不一致')
     } else {
-      callback();
+      if (confirmValue === value) setTimeout(setFieldsValue({ confirm: confirmValue }), 0)
+      callback()
     }
   }
-  function checkPasswordConfirm(rule, value, callback) {
+  function checkPasswordConfirm (rule, value, callback) {
     if (value && value !== getFieldValue('accountpwd')) {
-      callback('两次填写的密码不一致');
+      callback('两次填写的密码不一致')
     } else {
-      callback();
+      callback()
     }
   }
 
   return (
     <Modal
-      title="修改密码"
+      title='修改密码'
       visible={/password/.test(showModals)}
       onOk={handleOk}
       onCancel={handelCancel}
       afterClose={resetFields}
     >
       <Form>
-        <FormItem label="原密码">
+        <FormItem label='原密码'>
           {getFieldDecorator('orginpwd', {
             initialValue: '',
             rules: [{ required: true, message: '请输入原密码' }]
-          })(
-            <Input type="password" maxLength="16" placeholder="请输入您的原密码" />
-          )}
+          })(<Input type='password' maxLength='16' placeholder='请输入您的原密码' />)}
         </FormItem>
-        <FormItem label="新密码">
+        <FormItem label='新密码'>
           {getFieldDecorator('accountpwd', {
             initialValue: '',
-            rules: [{
-              required: true,
-              pattern: /^.{6,16}$/,
-              message: '密码为6-16位字符'
-            }, {
-              validator: checkSameChar
-            }]
+            rules: [
+              {
+                required: true,
+                pattern: /^.{6,16}$/,
+                message: '密码为6-16位字符'
+              },
+              {
+                validator: checkSameChar
+              }
+            ]
           })(
-            <Input onPaste={(e) => {
-              e.preventDefault();
-            }} type="password" maxLength="16" placeholder="请输入6-16位新密码" />
+            <Input
+              onPaste={e => {
+                e.preventDefault()
+              }}
+              type='password'
+              maxLength='16'
+              placeholder='请输入6-16位新密码'
+            />
           )}
         </FormItem>
-        <FormItem label="确认密码">
+        <FormItem label='确认密码'>
           {getFieldDecorator('confirm', {
             initialValue: '',
-            rules: [{
-              required: true, message: '请输入确认密码'
-            }, {
-              validator: checkPasswordConfirm
-            }]
+            rules: [
+              {
+                required: true,
+                message: '请输入确认密码'
+              },
+              {
+                validator: checkPasswordConfirm
+              }
+            ]
           })(
-            <Input onPaste={(e) => {
-              e.preventDefault();
-            }} type="password" maxLength="16" placeholder="请输入确认密码" />
+            <Input
+              onPaste={e => {
+                e.preventDefault()
+              }}
+              type='password'
+              maxLength='16'
+              placeholder='请输入确认密码'
+            />
           )}
         </FormItem>
       </Form>
     </Modal>
-  );
+  )
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps (dispatch) {
   return {
     modifyPwd: data => {
-      dispatch({ type: 'app/modifyPassword', payload: data });
+      dispatch({ type: 'app/modifyPassword', payload: data })
     },
     cancel: () => {
-      dispatch({ type: 'app/showModals', payload: '' });
+      dispatch({ type: 'app/showModals', payload: '' })
     }
-  };
+  }
 }
 export default connect(
   state => state.app,
   mapDispatchToProps
-)(Form.create()(ModifyPwdModal));
+)(Form.create({})(ModifyPwdModal))
