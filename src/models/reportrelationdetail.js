ab2ad21ac93c3e96ee6408f485ee8cedfcbc74d7
周靖人 <zhoujingren@wxchina.com>
2019-06-26 15:16:03 +0800

    汇报关系 - 编辑打开显示list bug fix

--- src/models/reportrelationdetail.js
+++ src/models/reportrelationdetail.js
@@ -1,8 +1,8 @@
-import { message } from 'antd';
-import { add, update, getreportreldetail, deletereportreldetail } from '../services/reportrelation';
-import { setSessionItem, getCacheData } from '../utils/newStorage';
+import { message } from 'antd'
+import { add, update, getreportreldetail, deletereportreldetail } from '../services/reportrelation'
+import { setSessionItem, getCacheData } from '../utils/newStorage'
 
-const NAMESPACE = 'reportrelationdetail';
+const NAMESPACE = 'reportrelationdetail'
 
 export default {
   namespace: NAMESPACE,
@@ -24,124 +24,124 @@ export default {
       pageIndex: 1,
       pageSize: 100000,
       searchOrder: '',
-      columnFilter: null //字段查询
+      columnFilter: null // 字段查询
     },
     formInfo: {
       fieldData: {}
     }
   },
   effects: {
-    *Init({ payload }, { put }) {
-      yield put({ type: 'putState', payload: { selectedRows: [] } });
-      yield put({ type: 'QueryList', payload });
+    * Init ({ payload }, { put }) {
+      yield put({ type: 'putState', payload: { selectedRows: [] } })
+      yield put({ type: 'QueryList', payload })
     },
-    *Search({ payload }, { put }) {
-      yield put({ type: 'setListParams', payload });
-      yield put({ type: 'QueryList' });
+    * Search ({ payload }, { put }) {
+      yield put({ type: 'setListParams', payload })
+      yield put({ type: 'QueryList' })
     },
-    *QueryList({ payload }, { select, put, call }) {
-      const { initParams } = yield select(state => state[NAMESPACE]);
-      const params = { ...(payload || initParams) };
+    * QueryList ({ payload }, { select, put, call }) {
+      const { initParams } = yield select(state => state[NAMESPACE])
+      const params = { ...(payload || initParams) }
 
       try {
-        const { data } = yield call(getreportreldetail, params);
-        const list = data.datalist || [];
-        const reportrelationid = sessionStorage.getItem('reportrelationid') || null;
+        const { data } = yield call(getreportreldetail, params)
+        const list = data.datalist || []
+        const reportrelationid = sessionStorage.getItem('reportrelationid') || null
 
-        yield put({ type: 'putState', payload: { list: list.filter(o => o.reportrelationid === reportrelationid) } });
+        yield put({ type: 'putState', payload: { list: list.filter(o => o.reportrelationid === reportrelationid) } })
       } catch (e) {
-        message.error(e.message || '获取列表失败');
+        message.error(e.message || '获取列表失败')
       }
     },
-    *Del({ payload }, { put, call }) {
+    * Del ({ payload }, { put, call }) {
       const params = {
         ReportRelDetailIds: payload,
         recstatus: 0
-      };
+      }
       try {
-        const { error_msg } = yield call(deletereportreldetail, params);
-        message.success(error_msg || '删除成功！');
-        yield put({ type: 'QueryList' });
+        const { error_msg } = yield call(deletereportreldetail, params)
+        message.success(error_msg || '删除成功！')
+        yield put({ type: 'QueryList' })
       } catch (e) {
-        message.error(e.message || '删除失败');
+        message.error(e.message || '删除失败')
       }
     },
-    *SubmitForm({ payload }, { select, call, put }) {
-      const { confirmLoading, recid } = yield select(state => state[NAMESPACE]);
-      const { params, resolve, isEdit } = payload;
+    * SubmitForm ({ payload }, { select, call, put }) {
+      const { confirmLoading, recid } = yield select(state => state[NAMESPACE])
+      const { params, resolve, isEdit } = payload
       try {
-        yield put({ type: 'handelLoading', payload: { ...confirmLoading, FormModal: true } });
+        yield put({ type: 'handelLoading', payload: { ...confirmLoading, FormModal: true } })
 
         const resultParams = {
           ...params,
           recid: recid || null
-        };
-        const response = yield call((!isEdit ? add : update), resultParams);
-        if (resolve) resolve(response);
-        yield put({ type: 'QueryList' });
-        yield put({ type: 'handelLoading', payload: { ...confirmLoading, FormModal: false } });
+        }
+        const response = yield call(!isEdit ? add : update, resultParams)
+        if (resolve) resolve(response)
+        yield put({ type: 'QueryList' })
+        yield put({ type: 'handelLoading', payload: { ...confirmLoading, FormModal: false } })
       } catch (e) {
-        yield put({ type: 'handelLoading', payload: { ...confirmLoading, FormModal: false } });
-        message.error(e.message);
+        yield put({ type: 'handelLoading', payload: { ...confirmLoading, FormModal: false } })
+        message.error(e.message)
       }
     },
-    *FecthAllFormData({ payload }, { select, call, put }) {
-      const { fetchDataLoading } = yield select(state => state[NAMESPACE]);
-      const { recid, resolve } = payload;
+    * FecthAllFormData ({ payload }, { select, call, put }) {
+      const { fetchDataLoading } = yield select(state => state[NAMESPACE])
+      const { recid, resolve } = payload
 
       try {
-        if (recid) { // 编辑
+        if (recid) {
+          // 编辑
           yield put({
             type: 'putState',
             payload: { fetchDataLoading: { ...fetchDataLoading, FormModal: true } }
-          });
+          })
 
-          const { data } = yield call(getreportreldetail, {});
-          const list = data.datalist || [];
+          const { data } = yield call(getreportreldetail, {})
+          const list = data.datalist || []
           yield put({
             type: 'putState',
             payload: {
-              list,
               selectedRows: list.filter(o => o.reportreldetailid === recid),
               fetchDataLoading: { ...fetchDataLoading, FormModal: false }
             }
-          });
+          })
         }
 
-        if (resolve) resolve({});
+        if (resolve) resolve({})
       } catch (e) {
         yield put({
           type: 'putState',
           payload: { fetchDataLoading: { ...fetchDataLoading, FormModal: false } }
-        });
-        message.error(e.message);
+        })
+        message.error(e.message)
       }
     }
   },
   reducers: {
-    putState(state, { payload: stateAssignment }) {
+    putState (state, { payload: stateAssignment }) {
       return {
         ...state,
         ...stateAssignment
-      };
+      }
     },
-    setListParams(state, { payload }) {
-      const key = `${NAMESPACE}_Params`;
-      const cacheKey = `cache${NAMESPACE}_Params`;
-      const cacheData = getCacheData(cacheKey, state.initParams);
+    setListParams (state, { payload }) {
+      const key = `${NAMESPACE}_Params`
+      const cacheKey = `cache${NAMESPACE}_Params`
+      const cacheData = getCacheData(cacheKey, state.initParams)
 
-      setSessionItem(cacheKey, cacheData);
-      setSessionItem(key, payload);
+      setSessionItem(cacheKey, cacheData)
+      setSessionItem(key, payload)
       return {
         ...state,
         initParams: payload
-      };
+      }
     },
-    handelLoading(state, { payload }) {
-      return { ...state, confirmLoading: payload };
+    handelLoading (state, { payload }) {
+      return { ...state, confirmLoading: payload }
     },
-    showModals(state, { payload }) {
-      return { ...state, showModals: payload };
+    showModals (state, { payload }) {
+      return { ...state, showModals: payload }
     }
   }
-};
+}
