fa75084e1301e44ba61c025faae4a426bf3ca3fd
周靖人 <zhoujingren@wxchina.com>
2019-06-26 18:19:18 +0800

    configTable 修改 为本地 搜索

--- src/components/ConfigTable/index.js
+++ src/components/ConfigTable/index.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import { Table } from 'antd';
-import { getColumns } from './columns';
-import { Option } from './formData';
-import { LoopList } from './columns/columns';
+import React, { Component } from 'react'
+import { Table } from 'antd'
+import { getColumns } from './columns'
+import { Option } from './formData'
+import { LoopList } from './columns/columns'
 
-const initClientHeight = document.body.offsetHeight && document.documentElement.clientHeight;
-const tableDomWdith = document.body.offsetWidth && document.documentElement.clientWidth - 300;
+const initClientHeight = document.body.offsetHeight && document.documentElement.clientHeight
+const tableDomWdith = document.body.offsetWidth && document.documentElement.clientWidth - 300
 
 class ConfigTable extends Component {
-  constructor(props) {
-    super(props);
-    const selectedRowKeys = props.selectedRows && props.rowKey ? props.selectedRows.map(o => o[props.rowKey]) : [];
+  constructor (props) {
+    super(props)
+    const selectedRowKeys = props.selectedRows && props.rowKey ? props.selectedRows.map(o => o[props.rowKey]) : []
 
     this.state = {
       clientHeight: initClientHeight,
@@ -18,113 +18,112 @@ class ConfigTable extends Component {
       selectedRowKeys,
       selectedRows: props.selectedRows || [],
       OptionList: Option
-    };
-    this.tableWidth = 1080;
-    this.onWindowResize = this.onWindowResize.bind(this);
+    }
+    this.tableWidth = 1080
+    this.onWindowResize = this.onWindowResize.bind(this)
   }
 
-  setStateAsync = state => new Promise(resolve => this.setState(state, resolve));
+  setStateAsync = state => new Promise(resolve => this.setState(state, resolve))
 
-  componentDidMount() {
-    window.addEventListener('resize', this.onWindowResize, false);
+  componentDidMount () {
+    window.addEventListener('resize', this.onWindowResize, false)
   }
 
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.onWindowResize, false);
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.onWindowResize, false)
   }
 
-  componentWillReceiveProps(nextProps) {
-    const { selectedRows: oldSelectedRows, rowKey } = this.props;
-    const { selectedRows } = nextProps;
+  componentWillReceiveProps (nextProps) {
+    const { selectedRows: oldSelectedRows, rowKey } = this.props
+    const { selectedRows } = nextProps
 
     if (selectedRows && rowKey && selectedRows.length !== oldSelectedRows.length) {
-      const selectedRowKeys = selectedRows.map(o => o[rowKey]);
-      this.setState({ selectedRowKeys, selectedRows });
+      const selectedRowKeys = selectedRows.map(o => o[rowKey])
+      this.setState({ selectedRowKeys, selectedRows })
     }
   }
 
-  onWindowResize() {
-    const clientHeight = document.body.offsetHeight && document.documentElement.clientHeight;
-    this.setState({ clientHeight });
+  onWindowResize () {
+    const clientHeight = document.body.offsetHeight && document.documentElement.clientHeight
+    this.setState({ clientHeight })
   }
 
-  importTableParmas() {
-    const { initParams: { searchOrder, columnFilter } } = this.props;
-    const { FilterVisibles } = this.state;
-    const toggleFilter = this.toggleFilter;
-    const onFilter = this.onFilter;
-    return { onFilter, toggleFilter, columnFilter, FilterVisibles, searchOrder };
+  importTableParmas () {
+    const {
+      initParams: { searchOrder, columnFilter }
+    } = this.props
+    const { FilterVisibles } = this.state
+    const toggleFilter = this.toggleFilter
+    const onFilter = this.onFilter
+    return { onFilter, toggleFilter, columnFilter, FilterVisibles, searchOrder }
   }
 
   // 设置过滤值
   onFilter = async (fieldname, value) => {
-    const { onSeach, initParams } = this.props;
-    const { FilterVisibles } = this.state;
+    const { onSeach, initParams } = this.props
+    const { FilterVisibles } = this.state
 
     const newFilterVisibles = {
       ...FilterVisibles,
       [fieldname]: false
-    };
-    await this.setStateAsync({ FilterVisibles: newFilterVisibles });
+    }
+    await this.setStateAsync({ FilterVisibles: newFilterVisibles })
 
     const newcolumnFilter = {
       ...initParams.columnFilter,
       [fieldname]: value
-    };
-    onSeach({ ...initParams, columnFilter: newcolumnFilter });
+    }
+    onSeach({ ...initParams, columnFilter: newcolumnFilter })
   }
 
   // 控制过滤组件
   toggleFilter = (fieldname, value) => {
-    const { FilterVisibles } = this.state;
-    const newFilters = { ...FilterVisibles };
-    Object.keys(newFilters).forEach(info => newFilters[info] = false);
-    newFilters[fieldname] = value;
-    this.setState({ FilterVisibles: newFilters });
+    const { FilterVisibles } = this.state
+    const newFilters = { ...FilterVisibles }
+    Object.keys(newFilters).forEach(info => (newFilters[info] = false))
+    newFilters[fieldname] = value
+    this.setState({ FilterVisibles: newFilters })
   }
 
   handleTableChange = (pagination, filters, sorter) => {
-    const { onSeach, initParams } = this.props;
-    const searchOrder = sorter.field ? (sorter.field + (sorter.order === 'ascend' ? ' asc' : ' desc')) : '';
+    const { onSeach, initParams } = this.props
+    const searchOrder = sorter.field ? sorter.field + (sorter.order === 'ascend' ? ' asc' : ' desc') : ''
     const newPramas = {
       ...initParams,
-      pageIndex: pagination.current,
-      pageSize: pagination.pageSize,
+      // pageIndex: pagination.current,
+      // pageSize: pagination.pageSize,
       searchOrder
-    };
-    onSeach(newPramas);
+    }
+    onSeach(newPramas)
   }
 
   onSelectListChange = async (selectedRowKeys, selectedRows) => {
-    const { CBSelectRow } = this.props;
+    const { CBSelectRow } = this.props
 
-    await this.setStateAsync({ selectedRowKeys, selectedRows });
-    if (CBSelectRow) CBSelectRow(selectedRows);
+    await this.setStateAsync({ selectedRowKeys, selectedRows })
+    if (CBSelectRow) CBSelectRow(selectedRows)
   }
 
   onSelectAllListChange = async (selected, selectedRows) => {
-    const { rowKey, CBSelectRow } = this.props;
-    const selectedRowKeys = selectedRows.map(o => o[rowKey]);
+    const { rowKey, CBSelectRow } = this.props
+    const selectedRowKeys = selectedRows.map(o => o[rowKey])
 
-    await this.setStateAsync({ selectedRowKeys, selectedRows });
-    if (CBSelectRow) CBSelectRow(selectedRows);
+    await this.setStateAsync({ selectedRowKeys, selectedRows })
+    if (CBSelectRow) CBSelectRow(selectedRows)
   }
 
-  render() {
-    const {
-      dataSource, rowKey = 'recid', columns: propColumns,
-      rowSelect, rowSelection
-    } = this.props;
-    const { selectedRowKeys, clientHeight } = this.state;
+  render () {
+    const { dataSource, rowKey = 'recid', columns: propColumns, rowSelect, rowSelection } = this.props
+    const { selectedRowKeys, clientHeight } = this.state
 
-    const screenHeight = clientHeight;
-    const modalHeight = screenHeight * 0.7;
-    const Div1Height = 15;
-    const Div2Height = 50;
-    const Div3Height = modalHeight - Div1Height - Div2Height;
+    const screenHeight = clientHeight
+    const modalHeight = screenHeight * 0.7
+    const Div1Height = 15
+    const Div2Height = 50
+    const Div3Height = modalHeight - Div1Height - Div2Height
 
-    const columnParams = this.importTableParmas();
-    const columns = getColumns(columnParams, (propColumns || LoopList), width => this.tableWidth = width);
+    const columnParams = this.importTableParmas()
+    const columns = getColumns(columnParams, propColumns || LoopList, width => (this.tableWidth = width))
 
     return (
       <Table
@@ -133,18 +132,22 @@ class ConfigTable extends Component {
         dataSource={dataSource}
         onChange={this.handleTableChange}
         scroll={{
-          x: (tableDomWdith > this.tableWidth ? '100%' : (rowSelect ? this.tableWidth + 62 : this.tableWidth)),
+          x: tableDomWdith > this.tableWidth ? '100%' : rowSelect ? this.tableWidth + 62 : this.tableWidth,
           y: Div3Height - 130
         }}
-        rowSelection={rowSelect ? {
-          selectedRowKeys,
-          onChange: this.onSelectListChange,
-          onSelectAll: this.onSelectAllListChange,
-          ...rowSelection
-        } : null}
+        rowSelection={
+          rowSelect
+            ? {
+              selectedRowKeys,
+              onChange: this.onSelectListChange,
+              onSelectAll: this.onSelectAllListChange,
+              ...rowSelection
+            }
+            : null
+        }
       />
-    );
+    )
   }
 }
 
-export default ConfigTable;
+export default ConfigTable
