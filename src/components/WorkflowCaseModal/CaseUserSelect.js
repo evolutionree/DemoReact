458c66a7cb9a9f2767a3222ce2e0cd950a2fcb11
周靖人 <zhoujingren@wxchina.com>
2019-06-26 18:32:03 +0800

    审批流 选人控件 选人后不显示选中人员bug  fix

--- src/components/WorkflowCaseModal/CaseUserSelect.js
+++ src/components/WorkflowCaseModal/CaseUserSelect.js
@@ -1,9 +1,9 @@
-import React, { PropTypes, Component } from 'react';
-import _ from 'lodash';
-import { Icon } from 'antd';
-import Avatar from '../Avatar';
-import UserSelectModal from './UserSelectModal';
-import styles from './styles.less';
+import React, { PropTypes, Component } from 'react'
+import _ from 'lodash'
+import { Icon } from 'antd'
+import Avatar from '../Avatar'
+import UserSelectModal from './UserSelectModal'
+import styles from './styles.less'
 
 class CaseUserSelect extends React.Component {
   static propTypes = {
@@ -13,51 +13,52 @@ class CaseUserSelect extends React.Component {
     limit: PropTypes.number,
     disabled: PropTypes.bool,
     isFreeUser: PropTypes.bool
-  };
+  }
   static defaultProps = {
     disabled: false,
     isFreeUser: true
-  };
+  }
 
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       modalVisible: false,
       showRemove: false
-    };
+    }
   }
 
-  handleOk = (userids) => {
-    this.hideModal();
-    this.props.onChange(userids);
-  };
+  handleOk = userids => {
+    this.hideModal()
+    this.props.onChange(userids)
+  }
 
   showModal = () => {
-    this.setState({ modalVisible: true });
-  };
+    this.setState({ modalVisible: true })
+  }
 
   hideModal = () => {
-    this.setState({ modalVisible: false });
-  };
+    this.setState({ modalVisible: false })
+  }
 
   toggleRemove = () => {
     this.setState(prevState => {
       return {
         ...prevState,
         showRemove: !prevState.showRemove
-      };
-    });
-  };
+      }
+    })
+  }
 
-  handleRemove = (item) => {
-    this.props.onChange(this.props.value.filter(id => id !== item.userid));
-  };
+  handleRemove = item => {
+    this.props.onChange(this.props.value.filter(id => id !== item.userid))
+  }
 
-  render() {
-    const { value, users, allUsers, limit, disabled } = this.props;
-    let selectedUsers = [];
+  render () {
+    const { value, users, allUsers, limit, disabled } = this.props
+
+    let selectedUsers = []
     if (value && value.length) {
-      selectedUsers = value.map(userId => _.find(users, ['userid', userId])).filter(u => !!u);
+      selectedUsers = value.map(userId => _.find(allUsers, ['userid', userId])).filter(u => !!u)
     }
     return (
       <div className={styles.fieldwrap}>
@@ -71,21 +72,25 @@ class CaseUserSelect extends React.Component {
               <span title={user.username}>{user.username}</span>
               {this.state.showRemove && (
                 <Icon
-                  type="close-circle"
+                  type='close-circle'
                   className={styles.remove}
-                  onClick={() => { this.handleRemove(user); }}
+                  onClick={() => {
+                    this.handleRemove(user)
+                  }}
                 />
               )}
             </li>
           ))}
           {(!limit || selectedUsers.length < limit) && !disabled && (
             <li onClick={this.showModal}>
-              <img src="/img_icon_add.png" alt="" style={{ cursor: 'pointer' }} />
+              <img src='/img_icon_add.png' alt='' style={{ cursor: 'pointer' }} />
             </li>
           )}
-          {!disabled && <li onClick={this.toggleRemove}>
-            <img src="/img_icon_delete.png" alt="" style={{ cursor: 'pointer' }} />
-          </li>}
+          {!disabled && (
+            <li onClick={this.toggleRemove}>
+              <img src='/img_icon_delete.png' alt='' style={{ cursor: 'pointer' }} />
+            </li>
+          )}
         </ul>
         <UserSelectModal
           visible={this.state.modalVisible}
@@ -99,8 +104,8 @@ class CaseUserSelect extends React.Component {
           zIndex={1002}
         />
       </div>
-    );
+    )
   }
 }
 
-export default CaseUserSelect;
+export default CaseUserSelect
