efbcce27421c70ef5ea4ae7b776a219b35e411c9
PC-202008141306\Administrator <zhaiqiuqiu@wxchina.com>
2021-07-01 12:08:46 +0800

    客户名称特殊显示

--- src/components/DynamicForm/controls/InputRecName.js
+++ src/components/DynamicForm/controls/InputRecName.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { createNormalInput } from './utils';
 import InputCustomerRecName from './InputCustomerRecName';
 import { queryFields } from '../../../services/entity';
+import InputRecManage from './InputRecManage';
 
 const customerEntityId = 'f9db9d79-e94b-4678-a5cc-aa6e281c1246'; // 客户实体id
 const xiansuoNameFieldId = 'e61403f1-4511-49b9-a1c1-52a8cea855d1'; //线索名称的fileldId
@@ -48,6 +49,8 @@ class InputRecName extends Component {
 
     const isXianSuo = fieldId === xiansuoNameFieldId
     const isOriginal = isCustomer || isXianSuo
+    console.log('test1111');
+    if (isCustomer) return <InputRecManage {...this.props} />;
 
     //客户新增 与 其他模块的新增 有差异   客户新增的时候  客户名称键入后需要请求接口 查询重复引用
     return (isOriginal || fieldname === 'recname') ?
