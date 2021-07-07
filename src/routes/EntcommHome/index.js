2566fb9c34d9141c5a0ec0771008e9ccb65aab0c
PC-202008141306\Administrator <zhaiqiuqiu@wxchina.com>
2021-07-07 11:52:09 +0800

    联系人决策树职能控制

--- src/routes/EntcommHome/index.js
+++ src/routes/EntcommHome/index.js
@@ -45,7 +45,7 @@ function EntcommHome({
           <LinkTab key={t.relid} to={`/entcomm/${entityId}/${recordId}/rel/${t.relid}/${t.relentityid}`}><IntlText name="relname" value={t} /></LinkTab>
         );
       })]}
-      {entityId === 'f9db9d79-e94b-4678-a5cc-aa6e281c1246' && <LinkTab to={`/entcomm/${entityId}/${recordId}/contactRelate`}>联系人决策树</LinkTab>}
+      {/* {entityId === 'f9db9d79-e94b-4678-a5cc-aa6e281c1246' && <LinkTab to={`/entcomm/${entityId}/${recordId}/contactRelate`}>联系人决策树</LinkTab>} */}
     </LinkTab.Group>
   );
 
