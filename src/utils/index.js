9511a7591bd4e5162b448873c33a0d67abdc4519
PC-202008141306\Administrator <zhaiqiuqiu@wxchina.com>
2021-09-18 17:22:06 +0800

    周报导出

--- src/utils/index.js
+++ src/utils/index.js
@@ -121,6 +121,15 @@ export function getCurrentYearFirstDay() {
   return moment().startOf('year').format('YYYY-MM-DD');
 }
 
+export function  getDateString(date, AddDayCount) { 
+  var  dd = new Date(date);
+  dd.setDate(dd.getDate()+AddDayCount); //获取AddDayCount天后的日期
+  var  y = dd.getFullYear(); 
+  var  m = (dd.getMonth()+1)<10? "0" +(dd.getMonth()+1):(dd.getMonth()+1); //获取当前月份的日期，不足10补0
+  var  d = dd.getDate()<10? "0" +dd.getDate():dd.getDate(); //获取当前几号，不足10补0
+  return  y+ "-" +m+ "-" +d; 
+}
+
 /*
  返回时间的时间戳
  */
